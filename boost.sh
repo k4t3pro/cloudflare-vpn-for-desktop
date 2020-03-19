f39893f667b689898df78d3d5243b566d5b5b9d5
k4t3pro <>
2020-03-19 09:51:24 +0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -6,7 +6,7 @@
 # -------------------
 referal=$1
 interval=$2
-chmod 777 /boost.sh; \
+
 docker run -v `pwd`/getdata.sh:/getdata.sh --rm -i alpine sh -c \
 "apk add pwgen curl; \
 chmod 777 /getdata.sh; \
